b4856a3281c6dd5ed981bffb280798c6aad4fc59
Jeruk99 <vlad_ladzd@mail.ru>
2022-05-13 20:57:26 +0400

    Добавьте файлы проекта.

---
+++ Dtree.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDTree
+{
+    class Dtree
+    {
+        private DTreeNode root;
+        public DTreeNode Root
+        {
+            get { return root; }
+            set { root = value; }
+        }
+        public Dtree()
+        {
+            root = null;
+        }
+        public DTreeNode DFind(int key, DTreeNode root)
+        {
+            DTreeNode a;
+            if (root == null) a = null;
+            {
+                if (key < root.Key) a = DFind(key, root.Left);
+                else if (key > root.Key) a = DFind(key, root.Right);
+                else a = root;
+            }
+            return a;
+        }
+        public DTreeNode InsertFind(int key, DTreeNode root)
+        {
+            if (root != null)
+            {
+                if (key < root.Key) root.Left = InsertFind(key, root.Left);
+                else if (key > root.Key) root.Right = InsertFind(key, root.Right);
+            }
+            else
+            {
+                char a;
+                char.TryParse(Console.ReadLine(), out a);
+                root = new DTreeNode(a, key, null, null);
+            }
+            return root;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DDTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+    class DTreeNode
+    {
+        private int key;
+        private char info;
+        private DTreeNode left;
+        private DTreeNode right;
+        public char Info
+        {
+            get { return info; }
+            set { info = value; }
+        }
+        public int Key
+        {
+            get { return key; }
+            set { key = value; }
+        } // свойства
+        public DTreeNode Left
+        {
+            get { return left; }
+            set { left = value; }
+        }
+        public DTreeNode Right
+        {
+            get { return right; }
+            set { right = value; }
+        }
+        public DTreeNode() { }
+        public DTreeNode(char info, int key)
+        {
+            Info = info; Key = key;
+        }
+        public DTreeNode(char info, int key, DTreeNode left, DTreeNode right)
+        {
+            Info = info; Key = key; Left = left; Right = right;
+        }
+    }
+}
+    
+
